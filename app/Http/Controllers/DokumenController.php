d1b6a4538a3c90ce971c048fe2fb8fd9d6441783
Hafis Ardiyanto <hafisardiyanto19@gmail.com>
2025-07-12 13:35:21 +0700

    labdjkhas

--- app/Http/Controllers/DokumenController.php
+++ app/Http/Controllers/DokumenController.php
@@ -84,7 +84,10 @@ class DokumenController extends Controller
             
             
       
-    
+nama_dokumen
+        ]);
+        return redirect('/dokumen')->with('success', 'Dokumen Berhasil Diupdate');
+    }   
     public function delete($id)
     {
         $dokumen = Sip::findOrFail($id);
